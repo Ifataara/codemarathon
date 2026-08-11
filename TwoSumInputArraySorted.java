9243381d90720dc37fe4f9f07c9816d13aaab418
Ifataara <ifatnalband@gmail.com>
2026-08-11 17:19:34 +0530

    Update TwoSumInputArraySorted.java

--- TwoSumInputArraySorted.java
+++ TwoSumInputArraySorted.java
@@ -1,19 +1,12 @@
-class Solution {
+public class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int l = 0;
-        int r = numbers.length-1;
-        while(l < r){
-            int sum = numbers[l]+numbers[r];
-
-            if(sum == target){
-
-                return new int[]{ l+1,r+1};
-            }else if (sum < target){
-                l++;
-            }else{
-                r--;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    return new int[] { i + 1, j + 1 };
+                }
             }
         }
-       return new int[]{}; 
+        return new int[0];
     }
 }
