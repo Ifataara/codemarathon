a561e3d890be30d8fc44a32c4d3df75a1fa81d82
Ifataara <ifatnalband@gmail.com>
2026-08-11 23:13:21 +0530

    Create 933 RecentCounter.java

---
+++ 933 RecentCounter.java
@@ -0,0 +1,18 @@
+class RecentCounter {
+    private Queue<Integer> requests;
+
+    public RecentCounter() {
+        this.requests = new LinkedList<>();
+        
+    }
+    
+    public int ping(int t) {
+        requests.offer(t);
+
+        while(!requests.isEmpty() && requests.peek()<t-3000){
+            requests.poll();
+        }
+        return requests.size();
+        
+    }
+}
