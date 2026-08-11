4ba4136716a9b6a219eaec19df1c760eec2c9813
Ifataara <ifatnalband@gmail.com>
2026-08-11 15:47:09 +0530

    Create IsAnagram.java

---
+++ IsAnagram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] aChar = s.toCharArray();
+        char[] bChar = t.toCharArray();
+        Arrays.sort(aChar);
+        Arrays.sort(bChar);
+        return Arrays.equals(aChar,bChar);
+
+    }
+}
